594623d3dabbfe483f452b83dd6c977db1475535
kenshinnet <kenshin.net@gmail.com>
2013-06-06 17:40:06 +0800

    修改了验证报错的问题

--- org.eclipse.bpmn2.modeler.core/src/org/eclipse/bpmn2/modeler/core/validation/ValidIdConstraint.java
+++ org.eclipse.bpmn2.modeler.core/src/org/eclipse/bpmn2/modeler/core/validation/ValidIdConstraint.java
@@ -12,11 +12,8 @@
  ******************************************************************************/
 package org.eclipse.bpmn2.modeler.core.validation;
 
-import org.eclipse.bpmn2.BaseElement;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.validation.AbstractModelConstraint;
-import org.eclipse.emf.validation.EMFEventType;
 import org.eclipse.emf.validation.IValidationContext;
 
 public class ValidIdConstraint extends AbstractModelConstraint {
@@ -24,30 +21,8 @@ public class ValidIdConstraint extends AbstractModelConstraint {
 	@Override
 	public IStatus validate(IValidationContext ctx) {
 		
-		//kenshin 修改    去掉了不需要的验证
-		/*
-		EObject eObj = ctx.getTarget();
-		EMFEventType eType = ctx.getEventType();
 		
-		// In the case of batch mode.
-		if (eType == EMFEventType.NULL) {
-			String id = null;
-			if (eObj instanceof BaseElement) {
-				id = ((BaseElement)eObj).getId(); 
-			}
-			
-			if (!SyntaxCheckerUtils.isNCName(id)) {
-				return ctx.createFailureStatus(new Object[] {eObj.eClass().getName()});
-			}
-		// In the case of live mode.
-		} else {
-			String newValue = (String) ctx.getFeatureNewValue();
-			
-			if (!SyntaxCheckerUtils.isNCName(newValue)) {
-				return ctx.createFailureStatus(new Object[] {eObj.eClass().getName()});
-			}
-		}
-		*/
+		
 		return ctx.createSuccessStatus();
 	}
 
